69eff1227d45959a58bd36179555495b0c79d944
Preston Doris <prestondoris@gmail.com>
2019-07-22 08:26:12 -0700

    Completed Asset Management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
-    return element;
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  
+  return element;
 }
 
 document.body.appendChild(component());
